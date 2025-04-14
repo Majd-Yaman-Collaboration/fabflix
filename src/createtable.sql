5a73afd57d7deb2e84599a6318a2dc8b8a3b226d
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-04-13 21:44:01 -0700

    movie-data and createtable. we're not really moving forward but we went backwards and now retracting our steps :sob:

--- src/createtable.sql
+++ src/createtable.sql
@@ -44,7 +44,7 @@ CREATE TABLE customers(
                           id INTEGER AUTO_INCREMENT PRIMARY KEY,
                           firstName VARCHAR(50),
                           lastName VARCHAR(50),
-                          ccid VARCHAR(20) UNIQUE,
+                          ccid VARCHAR(20),
                           address VARCHAR(200),
                           email VARCHAR(50),
                           password VARCHAR(20),
