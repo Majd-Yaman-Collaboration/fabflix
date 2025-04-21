3dee1a25aac47268a7787998f5e0efad07deec7d
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-04-20 19:11:11 -0700

    still doesn't work but getting better errors in inspect

--- WebContent/scripts/login.js
+++ WebContent/scripts/login.js
@@ -1,3 +1,28 @@
+const form = document.getElementById("form");
+const elements = form.elements;
+
+const errorDisplay = document.getElementById("error");
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    let email = elements["email"];
+    let password = elements["password"];
+
+    jQuery.ajax({
+        dataType: "json",
+        method: "POST",
+        url: "api/login",
+        data: {
+            email: email,
+            password: password
+        },
+        success: handle_results
+    });
+});
+
+
 function handle_results(data)
 {
     //return true if there is an error
