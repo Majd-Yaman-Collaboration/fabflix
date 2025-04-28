5a55b1820e00affb850439895415bf2346cd3ec4
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-04-28 00:31:59 -0700

    some changes to confirmation servlet to make it more right and easier to use

--- src/ConfirmationServlet.java
+++ src/ConfirmationServlet.java
@@ -11,6 +11,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+//RECIEVES: customerId, movieId, saleDate
+//SENDS: sale id
+
+
+
 @WebServlet(name = "ConfirmationServlet", urlPatterns = "/api/confirmation-servlet")
 public class ConfirmationServlet extends BaseServlet
 {
@@ -21,7 +26,7 @@ public class ConfirmationServlet extends BaseServlet
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int customerId = Integer.parseInt(request.getParameter("customerId"));
+        int customerId = Integer.parseInt(request.getSession().getAttribute("id").toString());
         String movieId = request.getParameter("movieId");
         String saleDate = request.getParameter("saleDate");
 
@@ -42,6 +47,7 @@ public class ConfirmationServlet extends BaseServlet
 
             String id = rsId.getString("id");
             JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("id", id);
             out.write(jsonObject.toString());
 
         }
