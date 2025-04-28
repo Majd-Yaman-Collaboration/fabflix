b007154f1d4c9d356f17b8439edf460b52653c06
MajdKatabi <majdkatabi@gmail.com>
2025-04-27 23:07:28 -0700

    checkout.js send ajax

--- WebContent/scripts/checkout.js
+++ WebContent/scripts/checkout.js
@@ -1,3 +1,37 @@
+const form = document.getElementById("form");
+const element = form.elements;
+const errorDisplay = document.getElementById("error");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let firstName = element["firstName"].value;
+    let lastName = element["lastName"].value;
+    let cardNumber = element["cardNumber"].value;
+    let expirationDate = element["expirationDate"].value;
+
+    jQuery.ajax({
+        dataType: "json",
+        method: "POST",
+        url: "api/checkout-servlet",
+        data: {
+            firstName:firstName,
+            lastName:lastName,
+            cardNumber:cardNumber,
+            expirationDate:expirationDate
+        },
+        success: handle_results
+    });
+});
+
+function handle_results(data) {
+    if (data.status === "success") {
+        window.location.href = "confirmation.html"
+    }
+    else {
+        jQuery("#error").text("Incorrect Payment Info");
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('payment-form').addEventListener('submit', function(e) {
         e.preventDefault();
