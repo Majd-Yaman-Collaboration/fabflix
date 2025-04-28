f69abc1de9272bd7e06b0069a3a32d192c54818e
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-04-28 00:26:27 -0700

    did the java side of confirmationServlet

---
+++ src/ConfirmationServlet.java
@@ -0,0 +1,54 @@
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "ConfirmationServlet", urlPatterns = "/api/confirmation-servlet")
+public class ConfirmationServlet extends BaseServlet
+{
+    private static final long serialVersionUID = 1L;
+    private String insertQuery =
+            "INSERT INTO sales (customerId, movieId, saleDate) VALUES (?, ?, ?)";
+    private String idQuery = "SELECT id FROM sales WHERE movieId = ?";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int customerId = Integer.parseInt(request.getParameter("customerId"));
+        String movieId = request.getParameter("movieId");
+        String saleDate = request.getParameter("saleDate");
+
+        try (Connection conn = dataSource.getConnection(); PrintWriter out = response.getWriter())
+        {
+            PreparedStatement ps = conn.prepareStatement(insertQuery);
+            int q = 1;
+            ps.setInt(q++, customerId);
+            ps.setString(q++, movieId);
+            ps.setString(q, saleDate);
+            ResultSet rs = ps.executeQuery();
+
+            PreparedStatement psId = conn.prepareStatement(idQuery);
+            psId.setString(1, movieId);
+            ResultSet rsId = psId.executeQuery();
+
+            if (!rsId.next()) return;
+
+            String id = rsId.getString("id");
+            JsonObject jsonObject = new JsonObject();
+            out.write(jsonObject.toString());
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
