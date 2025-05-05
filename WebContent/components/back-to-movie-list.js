6382fbb537cd97e6f675262cd366f7a38de6e3c1
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-05-05 16:08:27 -0700

    simplied the movielist back button code

--- WebContent/components/back-to-movie-list.js
+++
@@ -1,9 +0,0 @@
-function goBackToSavedMovieList() {
-    const lastUrl = sessionStorage.getItem("lastMovieListUrl");
-    if (lastUrl) {
-        window.location.href = lastUrl;
-    } else {
-        // fallback in case it's missing (first time or cleared storage)
-        window.location.href = "movie-list.html";
-    }
-}
