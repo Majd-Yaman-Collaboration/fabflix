a94e27c59da5603fd526a1469ec61a410180f345
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-05-07 22:45:28 -0700

    LoginDashboardServlet Implemented

--- src/Dashboard/LoginDashboardServlet.java
+++ src/Dashboard/LoginDashboardServlet.java
@@ -1,28 +1,30 @@
-//package Dashboard;
-//
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import supers.BaseServlet;
-//
-//@WebServlet(name = "LoginDashboardServlet", urlPatterns = "/api/logindashboard")
-//public class LoginDashboardServlet extends BaseServlet implements LoginInterface {
-//    private static final long serialVersionUID = 1L;
-//
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-//
-//        String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
-//        implementRecaptcha(gRecaptchaResponse, response);
-//
-//        response.setContentType("application/json");
-//        String email = request.getParameter("email");
-//        String password = request.getParameter("password");
-//
-//        handle_email_and_password_errors(email, password, response);
-//
-//        String query = "SELECT id, password FROM customers WHERE email = ?";
-//        handle_login_verification_through_database(email,password,request,response,dataSource,query);
-//    }
-//}
+package Dashboard;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import supers.BaseServlet;
+import Interfaces.LoginInterface;
+
+@WebServlet(name = "LoginDashboardServlet", urlPatterns = "/api/login-dashboard")
+public class LoginDashboardServlet extends BaseServlet implements LoginInterface {
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+        String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+        implementRecaptcha(gRecaptchaResponse, response);
+
+        response.setContentType("application/json");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        handle_email_and_password_errors(email, password, response);
+
+        String query = "SELECT id, password FROM employees WHERE email = ?";
+        handle_login_verification_through_database(email,password,request,response,dataSource,query);
+    }
+}
