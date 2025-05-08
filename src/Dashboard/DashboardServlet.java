415299c9426e234ddc32a9e61cd1543891439274
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-05-07 23:23:55 -0700

    added the _dashboard/dashboard-login.html path a I think most of its functionality

--- src/Dashboard/DashboardServlet.java
+++ src/Dashboard/DashboardServlet.java
@@ -5,14 +5,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import supers.BaseServlet;
 
+import java.io.IOException;
+
 @WebServlet(name = "DashboardServet", urlPatterns = "/api/_dashboard")
 public class DashboardServlet extends BaseServlet
 {
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect("_dashboard/dashboard-login.html");
     }
 }
