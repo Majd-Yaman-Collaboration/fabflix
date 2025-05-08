4cb703ff40903251ae87c13f4e26a95ff0c47fb2
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-05-07 22:38:22 -0700

    added shoppingCartServlet to checkout package

--- src/Checkout/ShoppingCartServlet.java
+++ src/Checkout/ShoppingCartServlet.java
@@ -1,18 +1,13 @@
+package Checkout;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 import com.google.gson.Gson;
