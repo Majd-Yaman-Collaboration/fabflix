c17ea0c5a94029a841a34421f2175f12a40224e4
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-05-11 11:36:25 -0700

    added batch optimization and have a current working example inserting into the db (sort of excluding genres too)

--- src/ObjectClasses/Movie.java
+++ src/ObjectClasses/Movie.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 
 public class Movie
 {
-    public  String id = "";
 
     public  String title = "";
 
-    public  String year = "";
+    public int   year = 0;
 
     public  String director = "";
 
+    public double rating = 0;
+
     public ArrayList<String> genres;
 
+    //simply for testing and making sure I got everything
     public boolean valid()
     {
-        return !(id.isEmpty() || title.isEmpty() || year.isEmpty() || director.isEmpty());
+        return !(title.isEmpty() || year == 0 || director.isEmpty() || rating == 0);
     }
 
     public Movie()
@@ -24,10 +26,11 @@ public class Movie
 
     @Override
     public String toString() {
-        return  "id "       + id       + "|" +
+        return
                 "title "    + title    + "|" +
                 "year "     + year     + "|" +
                 "director " + director + "|" +
-                "genre "    + genres;
+                "genre "    + genres   + "|" +
+                "rating  "  + rating;
     }
 }
--- src/XMLParsing/mains243.java
+++ src/XMLParsing/mains243.java
@@ -1,7 +1,6 @@
 package XMLParsing;
 
 import ObjectClasses.Movie;
-import jakarta.servlet.http.HttpServlet;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -13,9 +12,12 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
+
+//TODO
 public class mains243 extends DefaultHandler
 {
     ArrayList<Movie> movieList = new ArrayList<Movie>();
@@ -24,19 +26,106 @@ public class mains243 extends DefaultHandler
     String current_director;
     private Movie current_movie;
 
+    int current_id = 0;
+    String max;
+    PreparedStatement ps;
+    ArrayList<String> generated_ids = new ArrayList<>();
+
 
     public static void main(String[] args)
     {
         mains243 obj = new mains243();
         obj.parseDocument();
-        obj.movieList.stream().skip(100).limit(20).forEach(System.out::println);
-        System.out.println(obj.movieList.size());
-        System.out.println(obj.movieList.stream().filter(Movie::valid).count());
-        obj.movieList.stream().filter(movie -> {
-            return !movie.valid();
-        }).forEach(System.out::println);
+
+        obj.insert_all_movies();
+
+        Scanner input = new Scanner(System.in);
+        input.nextLine(); //pause the program for a moment
+
+        obj.delete_all_movies();
 
 
+    }
+
+    private Connection get_connection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/moviedb",
+                    "root",
+                    "temporary123"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private void insert_all_movies()
+    {
+        String insert_query = "INSERT INTO movies VALUES (?,?,?,?)";
+        String get_max_id_query = "SELECT MAX(id) FROM movies";
+
+        try (Connection conn = get_connection())
+        {
+            assert conn != null;
+            conn.setAutoCommit(false);
+            PreparedStatement ps_getId = conn.prepareStatement(get_max_id_query);
+            ps = conn.prepareStatement(insert_query);
+            ResultSet rs_getId = ps_getId.executeQuery();
+
+            if (rs_getId.next()) max = rs_getId.getString(1); else max = "tt0000000";
+            current_id = Integer.parseInt(max.substring(2));
+
+            int batch_size = 500;
+            int count = 0;
+            for (Movie movie : movieList)
+            {
+                ++current_id;
+                max = "tt" + String.format("%07d",current_id);
+                generated_ids.add(max);
+
+
+                ps.setString(1,max);
+                ps.setString(2,movie.title);
+                ps.setInt(3,movie.year);
+                ps.setString(4,movie.director);
+                ps.addBatch();
+
+                if (++count % batch_size == 0)
+                {
+                    ps.executeBatch();
+                    ps.clearBatch();
+                }
+
+            }
+
+            ps.executeBatch();
+            conn.setAutoCommit(true);
+        }
+        catch (Exception e){e.printStackTrace();}
+
+    }
+
+    private void delete_all_movies()
+    {
+        String query = "DELETE FROM movies WHERE id = ?";
+
+        try (Connection conn = get_connection())
+        {
+            ps = conn.prepareStatement(query);
+            conn.setAutoCommit(false);
+            for (String id : generated_ids)
+            {
+                ps.setString(1,id);
+                ps.addBatch();
+            }
+            ps.executeBatch();
+            conn.commit();
+            conn.setAutoCommit(true);
+        }
+        catch (Exception e){e.printStackTrace();}
     }
 
     private void parseDocument() {
@@ -70,8 +159,6 @@ public class mains243 extends DefaultHandler
         {
             current_movie = new Movie();
         }
-
-
     }
 
     @Override
@@ -83,35 +170,39 @@ public class mains243 extends DefaultHandler
     public void endElement(String uri, String localName, String qName) throws SAXException
     {
 
-        if (qName.equalsIgnoreCase("t")) //title
-        {
+        switch (qName.toLowerCase()) {
+            case "t": // title
+                current_movie.title = element_content;
+                current_movie.director = current_director;
+                break;
 
-            current_movie.title = element_content;
-            current_movie.director = current_director;
-        }
-        else if (qName.equalsIgnoreCase("fid") || qName.equalsIgnoreCase("filmed"))
-        {
-            current_movie.id = element_content;
-        }
-        else if (qName.equalsIgnoreCase("year"))
-        {
-            current_movie.year = element_content;
-        }
-        else if (qName.equalsIgnoreCase("cat")) //single genre (category)
-        {
-            cats.add(element_content);
-        }
-        else if (qName.equalsIgnoreCase("cats")) //genres (categories)
-        {
-            current_movie.genres = cats;
-            cats = new ArrayList<>();
-            movieList.add(current_movie);
-        }
-        else if (qName.equalsIgnoreCase("dirname"))
-        {
-            current_director = element_content;
+            case "year":
+                try {
+                    current_movie.year = Integer.parseInt(element_content);
+                } catch (Exception e)
+                {
+                    current_movie.year = 0;
+                }
+                break;
 
+            case "cat": // single genre
+                cats.add(element_content);
+                break;
+
+            case "cats": // assign genres list
+                current_movie.genres = cats;
+                cats = new ArrayList<>();
+                break;
+
+            case "film": // add movie to list
+                movieList.add(current_movie);
+                break;
+
+            case "dirname": // director name
+                current_director = element_content;
+                break;
         }
+
 
     } //function end
 
