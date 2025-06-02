ad7bc2b23ef6572c70df5a440be6c948ff0e602a
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-06-02 14:34:03 -0700

    taking off the stuff in login fitler again

--- src/CustomerLogin/LoginFilter.java
+++ src/CustomerLogin/LoginFilter.java
@@ -52,8 +52,7 @@ public class LoginFilter implements Filter {
         boolean     loggedIn = (session != null) && (session.getAttribute("customer") != null);
 
 
-        loggedIn = true;
-        allowed = true;
+
         if (loggedIn || allowed)
             chain.doFilter(request, response); // allow access
         else
